4f45eb175c1ba6450d7eece41cb35e8cc7dc075e
Vlads <Vladsinitsa23@gmail.com>
2020-12-09 20:55:21 +0300

    fix datasource.properties

--- dao/src/main/java/org/example/vladsin/adverboard/dao/config/SettingsConfig.java
+++ dao/src/main/java/org/example/vladsin/adverboard/dao/config/SettingsConfig.java
@@ -11,7 +11,7 @@ import java.io.InputStream;
 import java.util.Properties;
 
 @Configuration
-@PropertySource("classpath:application.properties")
+@PropertySource("classpath:datasource.properties")
 public class SettingsConfig {
 
     @Bean
